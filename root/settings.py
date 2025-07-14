6eb2c0c876d83ba9da10bd6b05299066618d2a2f
Alixon <132573114+Alixon2010@users.noreply.github.com>
2025-07-14 13:23:17 +0500

    Добавлен whitenoise для статики

--- root/settings.py
+++ root/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['django2-4d1z.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://django2-4d1z.onrender.com']
 
 # Application definition
 
